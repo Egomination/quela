d3d3a23fb23875f65f2ca9467bd87d417d0978e3
sasalx <oyundanben@gmail.com>
2019-03-24 01:33:31 +0300

    Bloc Logic for Login

--- lib/bloc/auth/auth.dart
+++ lib/bloc/auth/auth.dart
@@ -1,9 +1,21 @@
 import 'dart:async';
 
+import 'package:meta/meta.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Auth {
   final storage = new FlutterSecureStorage();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<String> signIn({
+    @required String email,
+    @required String password,
+  }) async {
+    FirebaseUser user = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return user.uid;
+  }
 
   Future<void> deleteToken() async {
     await storage.delete(key: "token");
