65146d4af3ddd3a8b3e9289a62c62f120924d4c0
sasalx <oyundanben@gmail.com>
2019-03-24 00:38:48 +0300

    Auth States

---
+++ lib/bloc/auth/state.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthStates extends Equatable {}
+
+class BeforeAuth extends AuthStates {
+  // State where user is yet to see anything
+  // NOTE: A splash screen would be ideal
+  @override
+  String toString() => 'BeforeAuth';
+}
+
+class Authenticated extends AuthStates {
+  // State where user is authenticated
+  @override
+  String toString() => 'Authenticated';
+}
+
+class Unauthenticated extends AuthStates {
+  // State where user is unauthenticated
+  @override
+  String toString() => 'Unauthenticated';
+}
+
+class Loading extends AuthStates {
+  // State where user is authentication yet to be determined
+  @override
+  String toString() => 'Loading';
+}
