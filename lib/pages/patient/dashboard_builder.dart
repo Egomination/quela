74e3670a9b7a59f53d7c8f8479b201faade80c24
Taylan Dogan <doganntaylan@gmail.com>
2019-04-03 19:04:52 +0000

    implement: Doctor/Patient info for Patient/Doctor on RTC
    the initial idea was implementing a page for patients which allows them to see their doctors and their information. I decided to put that on RTC. So whenever patient's doctor(s) available on RTC server, cards color turns to the green (they're available for video chat). If they're offline, cards color will be red and call button will be disabled.
    
    I am also thinking that using this design as for doctor's dashboard. They'll see their patients information on these cards. There'll be an extra button that leads them to the patient's detailed information page.
    
    Signed-off-by: Taylan Dogan <doganntaylan@gmail.com>

--- lib/pages/patient/dashboard_builder.dart
+++ lib/pages/patient/dashboard_builder.dart
@@ -21,23 +21,14 @@ class PatientDashboardBuilder extends StatelessWidget {
             title: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Expanded(
-                  child: Text(
-                    "Patient Dashboard",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                      fontSize: 17.0,
-                    ),
+                Text(
+                  "Patient Dashboard",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                    fontSize: 17.0,
                   ),
                 ),
-                InkWell(
-                  onTap: () =>
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => TestList())),
-                  child: Icon(Icons.donut_small),
-                ),
-                Container(width: 20.0),
                 InkWell(
                   onTap: () => authBloc.dispatch(LoggedOut()),
                   child: Icon(Icons.power_settings_new),
@@ -58,99 +49,6 @@ class PatientDashboardBuilder extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class TestList extends StatelessWidget {
-  final imgURL =
-      "https://image.freepik.com/free-photo/doctor-smiling-with-stethoscope_1154-36.jpg";
-
-  Widget _testChildBuilder() {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.only(top: 45.0),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(50.0),
-            child: Image.network(
-              imgURL,
-              width: 150,
-              height: 150,
-              fit: BoxFit.fill,
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 25.0),
-          child: Text(
-            "Muhittin Topalak",
-            softWrap: true,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-        Text(
-          "Hearth and Brain Specialist",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.w400,
-            fontSize: 20.0,
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 34.0,
-            left: 4.0,
-            right: 4.0,
-          ),
-          child: Text(
-            "Note: Please use Video Call panel to call this doctor. It'll be"
-                " shown that whether the doctor is available or not.",
-            textAlign: TextAlign.center,
-            softWrap: true,
-            style: TextStyle(
-              fontStyle: FontStyle.italic,
-              fontSize: 16.0,
-              fontWeight: FontWeight.w300,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Doctor Info"),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.symmetric(
-          vertical: 50.0,
-          horizontal: 15.0,
-        ),
-        itemCount: 10,
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10.0),
-            width: 230.0,
-            height: 200.0,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(120.0)),
-            ),
-            child: _testChildBuilder(),
-          );
-        },
-      ),
     );
   }
 }
