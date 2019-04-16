4895459d07ac50be5389a44e99f84dc9149eddab
sasalx <oyundanben@gmail.com>
2019-04-16 21:54:55 +0300

    Patient Graph Test

--- lib/pages/doctor/doctor_patient.dart
+++ lib/pages/doctor/doctor_patient.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
 import 'package:quela/models/doctor.dart';
+import 'package:quela/pages/doctor/t.dart';
 import 'package:quela/utils/hex_code.dart';
 
 class DetailsPage extends StatelessWidget {
@@ -313,6 +316,32 @@ class DetailsPage extends StatelessWidget {
     );
   }
 
+  Widget _patientGraph(BuildContext context, PatientId patient) {
+    return TimeSeriesRangeAnnotationMarginChart(
+      _createSampleData(),
+      animate: false,
+    );
+  }
+
+  /// Create one series with sample hard coded data.
+  static List<charts.Series<TimeSeriesSales, DateTime>> _createSampleData() {
+    final data = [
+      new TimeSeriesSales(new DateTime(2017, 9, 19), 5),
+      new TimeSeriesSales(new DateTime(2017, 9, 26), 25),
+      new TimeSeriesSales(new DateTime(2017, 10, 3), 100),
+      new TimeSeriesSales(new DateTime(2017, 10, 10), 75),
+    ];
+
+    return [
+      new charts.Series<TimeSeriesSales, DateTime>(
+        id: 'Sales',
+        domainFn: (TimeSeriesSales sales, _) => sales.time,
+        measureFn: (TimeSeriesSales sales, _) => sales.sales,
+        data: data,
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -330,19 +359,24 @@ class DetailsPage extends StatelessWidget {
               child: _patientInfo(context, patient),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(left: 40.0),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height / 2.2,
-              decoration: BoxDecoration(
-                color: HexColor("#679287"),
-              ),
-              child: _patientData(context, patient),
-            ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 2.2,
+            decoration: BoxDecoration(
+                //color: HexColor("#679287"),
+                ),
+            child: _patientGraph(context, patient),
           ),
         ],
       ),
     );
   }
+}
+
+/// Sample time series data type.
+class TimeSeriesSales {
+  final DateTime time;
+  final int sales;
+
+  TimeSeriesSales(this.time, this.sales);
 }
